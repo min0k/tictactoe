167072f852bf81d07186e5132393065e5910c013
min0k <mino.kim@minokim.com>
2021-12-04 18:49:01 -0800

    create hardcoded mockup of playing board

--- js.js
+++ js.js
@@ -1,1 +1,27 @@
-console.log("hello");
+
+// Players
+const Player = (playerName) => {
+    let getName = console.log(playerName);
+
+    return {getName}
+};
+
+// Game Logic
+const ticTacToe = (() => {
+    const play = () => console.log("play was made");
+
+    return {play};
+})();
+
+
+// Game board
+const gameBoard = (() => {
+let gameBoardArray = [];
+
+return {gameBoardArray};
+
+})();
+
+const player1 = Player("mino");
+
+player1.getName;
