68ed2c42d37695c2a43a3d759e004ff9ba456e56
min0k <mino.kim@minokim.com>
2021-12-05 15:36:03 -0800

    added gameBoard functionality

--- js.js
+++ js.js
@@ -1,27 +1,49 @@
+// grab html elements
+
+const board = document.querySelector(".board-container");
+const boardTiles = document.querySelectorAll(".play-tile");
+
+
+
+
+
+
 
 // Players
 const Player = (playerName) => {
     let getName = console.log(playerName);
-
     return {getName}
 };
 
 // Game Logic
 const ticTacToe = (() => {
     const play = () => console.log("play was made");
-
     return {play};
 })();
 
 
 // Game board
 const gameBoard = (() => {
-let gameBoardArray = [];
+    
+const gameBoardArray = ["", "", "", "", "", "", "", "", ""];
 
-return {gameBoardArray};
+let updateGameBoard = (e) => {
+    console.log(e.target.dataset.value);
 
+    // Update to be X or O depending on which user
+    gameBoardArray[e.target.dataset.value] = "X";
+    console.log(gameBoardArray);
+
+    // Update to be X or O depending on which user
+    boardTiles[e.target.dataset.value].textContent = "X";
+}
+
+let addListenersToTiles = () => {
+    boardTiles.forEach(tile => tile.addEventListener("click", updateGameBoard)); 
+}
+
+addListenersToTiles();
+
+return {};
 })();
 
-const player1 = Player("mino");
-
-player1.getName;
